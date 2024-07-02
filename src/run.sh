a4896a5e92895dffda47d65e2830c588188b2727
Rohit Dutta <rohitdutta2510@gmail.com>
2024-07-02 15:18:39 +0530

    removed files

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,16 @@
 export CUDA_VISIBLE_DEVICES=1
 export TOKENIZERS_PARALLELISM=false
 
-python llmcascade_boolq_single_scorer.py
+
+
+for bs in 32;
+do
+    for dataset in mnli_test_1k;
+    do
+        for llm in google/flan-t5-base google/flan-t5-large google/flan-t5-xl;
+        do
+            python run_llm.py $llm ../datasets/$dataset.csv --out_dir ../temp_outs/ --max_gen_tokens 100 --bs $bs
+        done
+    done
+done
+
